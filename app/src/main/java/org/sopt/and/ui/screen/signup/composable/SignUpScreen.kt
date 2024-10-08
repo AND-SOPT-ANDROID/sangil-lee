6d00400406a4256a3d3d4ffe733152babab5dd00
Thirfir <sangbbang31@gmail.com>
2024-10-08 20:17:40 +0900

    add: 이메일 입력 caution field 추가

--- app/src/main/java/org/sopt/and/ui/screen/signup/composable/SignUpScreen.kt
+++ app/src/main/java/org/sopt/and/ui/screen/signup/composable/SignUpScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -19,6 +20,7 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.and.R
+import org.sopt.and.ui.component.text.IconFrontText
 import org.sopt.and.ui.component.textfield.FilledTextField
 import org.sopt.and.ui.theme.WavveTheme
 
@@ -68,6 +70,12 @@ fun SignUpScreen(
                 emailInput = it
             },
             placeholder = stringResource(R.string.sign_up_email_input_placeholder)
+        )
+
+        IconFrontText(
+            modifier = Modifier.padding(top = 8.dp),
+            text = stringResource(R.string.sign_up_caution_exact_email),
+            painter = painterResource(R.drawable.ic_caution),
         )
     }
 }
