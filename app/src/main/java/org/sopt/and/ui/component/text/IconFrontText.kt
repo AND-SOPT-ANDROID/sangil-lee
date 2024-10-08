3bd42c746068a2ef8130119d098da05c700e6ccc
Thirfir <sangbbang31@gmail.com>
2024-10-08 19:35:45 +0900

    add: 이메일 입력 caution string resource 추가

--- app/src/main/java/org/sopt/and/ui/component/text/IconFrontText.kt
+++ app/src/main/java/org/sopt/and/ui/component/text/IconFrontText.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -83,7 +84,7 @@ fun IconFrontText(
 @Preview
 private fun IconFrontTextPreview() {
     IconFrontText(
-        text = "로그인, 비밀번호 찾기, 알림에 사용되니 정확한 이메일을 입력해주세요.",
+        text = stringResource(R.string.sign_up_caution_exact_email),
         painter = painterResource(R.drawable.ic_caution),
     )
 }
