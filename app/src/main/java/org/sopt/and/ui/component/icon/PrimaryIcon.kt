71072799a3bf1e633cdc42fabb96ec807564d15c
Thirfir <sangbbang31@gmail.com>
2024-10-08 19:30:10 +0900

    add: Primary Color Icon 컴포넌트

---
+++ app/src/main/java/org/sopt/and/ui/component/icon/PrimaryIcon.kt
@@ -0,0 +1,23 @@
+package org.sopt.and.ui.component.icon
+
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import org.sopt.and.ui.theme.WavveTheme
+
+@Composable
+fun PrimaryIcon(
+    painter: Painter,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    tint: Color = WavveTheme.colorScheme.primary
+) {
+    Icon(
+        painter = painter,
+        contentDescription = contentDescription,
+        modifier = modifier,
+        tint = tint
+    )
+}
