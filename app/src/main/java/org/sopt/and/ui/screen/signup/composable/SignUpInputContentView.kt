699cdd622fe052520e6b30a1e0a7b3a4fde81620
Thirfir <sangbbang31@gmail.com>
2024-10-09 02:18:33 +0900

    add: Password Visibility trigger 추가

--- app/src/main/java/org/sopt/and/ui/screen/signup/composable/SignUpInputContentView.kt
+++ app/src/main/java/org/sopt/and/ui/screen/signup/composable/SignUpInputContentView.kt
@@ -1,17 +1,28 @@
 package org.sopt.and.ui.screen.signup.composable
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,6 +31,7 @@ import org.sopt.and.ui.component.divider.CenterContentHorizontalDivider
 import org.sopt.and.ui.component.row.SocialIconsRow
 import org.sopt.and.ui.component.text.IconFrontText
 import org.sopt.and.ui.component.text.TertiaryText
+import org.sopt.and.ui.component.text.WavvePrimaryText
 import org.sopt.and.ui.component.text.WavveSecondaryText
 import org.sopt.and.ui.component.textfield.FilledTextField
 import org.sopt.and.ui.theme.WavveTheme
@@ -32,6 +44,8 @@ fun SignUpInputContentView(
     onEmailInputChanged: (String) -> Unit,
     onPasswordInputChanged: (String) -> Unit
 ) {
+
+    var isPasswordVisible by remember { mutableStateOf(false) }
 
     Column(
         modifier = modifier
@@ -84,7 +98,15 @@ fun SignUpInputContentView(
             modifier = Modifier.padding(top = 20.dp).fillMaxWidth(),
             value = passwordInput,
             onValueChange = onPasswordInputChanged,
-            placeholder = stringResource(R.string.sign_up_password_input_placeholder)
+            placeholder = stringResource(R.string.sign_up_password_input_placeholder),
+            visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            trailingContent = {
+                WavvePrimaryText(
+                    modifier = Modifier.clickable { isPasswordVisible = !isPasswordVisible },
+                    text = if (isPasswordVisible) stringResource(R.string.hide_password_indicate) else stringResource(R.string.show_password_indicate)
+                )
+            }
         )
 
         IconFrontText(
