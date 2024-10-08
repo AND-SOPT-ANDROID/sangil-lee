1edc0ea69a3d27be9439fe011e9ba9395aed038a
Thirfir <sangbbang31@gmail.com>
2024-10-08 21:15:47 +0900

    refactor: SignUp 컴포저블 구조 수정

--- app/src/main/java/org/sopt/and/ui/screen/signup/composable/SignUpScreen.kt
+++ app/src/main/java/org/sopt/and/ui/screen/signup/composable/SignUpScreen.kt
@@ -1,138 +1,22 @@
 package org.sopt.and.ui.screen.signup.composable
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import org.sopt.and.R
-import org.sopt.and.ui.component.divider.CenterContentHorizontalDivider
-import org.sopt.and.ui.component.image.CircularImage
-import org.sopt.and.ui.component.row.SocialIconsRow
-import org.sopt.and.ui.component.text.IconFrontText
-import org.sopt.and.ui.component.text.TertiaryText
-import org.sopt.and.ui.component.text.WavveSecondaryText
-import org.sopt.and.ui.component.textfield.FilledTextField
-import org.sopt.and.ui.theme.WavveTheme
 
 @Composable
 fun SignUpScreen(
     modifier: Modifier = Modifier
 ) {
-
-    var emailInput by remember {
-        mutableStateOf("")
-    }
-    var passwordInput by remember {
-        mutableStateOf("")
-    }
-
-    Column(
-        modifier = modifier
-    ) {
-        Text(
-            text = buildAnnotatedString {
-                withStyle(style = SpanStyle(
-                    color = WavveTheme.colorScheme.primary
-                )) {
-                    append("이메일과 비밀번호")
-                }
-                withStyle(style = SpanStyle(
-                    color = WavveTheme.colorScheme.secondary
-                )) {
-                    append("만으로\n")
-                }
-                withStyle(style = SpanStyle(
-                    color = WavveTheme.colorScheme.primary
-                )) {
-                    append("Wavve를 즐길 수 ")
-                }
-                withStyle(style = SpanStyle(
-                    color = WavveTheme.colorScheme.secondary
-                )) {
-                    append("있어요!")
-                }
-            },
-            style = WavveTheme.typography.headSmall,
-            fontWeight = FontWeight.Medium
-        )
-
-        FilledTextField(
-            modifier = Modifier.padding(top = 24.dp).fillMaxWidth(),
-            value = emailInput,
-            onValueChange = {
-                emailInput = it
-            },
-            placeholder = stringResource(R.string.sign_up_email_input_placeholder)
-        )
-
-        IconFrontText(
-            modifier = Modifier.padding(top = 8.dp),
-            text = stringResource(R.string.sign_up_caution_exact_email),
-            painter = painterResource(R.drawable.ic_caution),
-        )
-
-        FilledTextField(
-            modifier = Modifier.padding(top = 16.dp).fillMaxWidth(),
-            value = passwordInput,
-            onValueChange = {
-                passwordInput = it
-            },
-            placeholder = stringResource(R.string.sign_up_password_input_placeholder)
-        )
-
-        IconFrontText(
-            modifier = Modifier.padding(top = 8.dp),
-            text = stringResource(R.string.sign_up_caution_password, 8, 20),
-            painter = painterResource(R.drawable.ic_caution),
-        )
-
-        CenterContentHorizontalDivider(
-            modifier = Modifier.padding(top = 48.dp)
-        ) {
-            WavveSecondaryText(
-                modifier = Modifier.padding(horizontal = 8.dp),
-                text = stringResource(R.string.sign_up_with_other),
-            )
-        }
-
-        SocialIconsRow(
-            modifier = Modifier.padding(top = 24.dp),
-            spacing = 12.dp
-        )
-
-        TertiaryText(
-            modifier = Modifier.padding(top = 30.dp),
-            text = stringResource(R.string.sign_up_alert_social)
-        )
-
-    }
+    SignUpInputContentView(
+        modifier = modifier.padding(top = 40.dp).padding(horizontal = 12.dp)
+    )
 }
 
-@Preview
 @Composable
+@Preview
 private fun SignUpScreenPreview() {
-    SignUpScreen(
-        modifier = Modifier.fillMaxSize()
-    )
+    SignUpScreen()
 }
