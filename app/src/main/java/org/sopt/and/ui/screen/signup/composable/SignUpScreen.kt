600d674e0cd9b0434a3784353fb51d40540a481f
Thirfir <sangbbang31@gmail.com>
2024-10-08 21:01:54 +0900

    add: 다른 소셜 계정으로 가입 Row

--- app/src/main/java/org/sopt/and/ui/screen/signup/composable/SignUpScreen.kt
+++ app/src/main/java/org/sopt/and/ui/screen/signup/composable/SignUpScreen.kt
@@ -1,15 +1,21 @@
 package org.sopt.and.ui.screen.signup.composable
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -21,6 +27,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.and.R
 import org.sopt.and.ui.component.divider.CenterContentHorizontalDivider
+import org.sopt.and.ui.component.image.CircularImage
+import org.sopt.and.ui.component.row.SocialIconsRow
 import org.sopt.and.ui.component.text.IconFrontText
 import org.sopt.and.ui.component.text.WavveSecondaryText
 import org.sopt.and.ui.component.textfield.FilledTextField
@@ -106,6 +114,12 @@ fun SignUpScreen(
                 text = stringResource(R.string.sign_up_with_other),
             )
         }
+
+        SocialIconsRow(
+            modifier = Modifier.padding(top = 24.dp),
+            spacing = 12.dp
+        )
+
     }
 }
 
