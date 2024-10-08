3a48c79f052c9c30a7aeb8275bce74971db44ef6
Thirfir <sangbbang31@gmail.com>
2024-10-09 02:09:14 +0900

    refactor: FilledTextField 컴포넌트에 trailing content 추가

--- app/src/main/java/org/sopt/and/ui/component/textfield/FilledTextField.kt
+++ app/src/main/java/org/sopt/and/ui/component/textfield/FilledTextField.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -12,6 +14,7 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -22,6 +25,7 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.and.ui.component.text.TextFieldPlaceholder
+import org.sopt.and.ui.component.text.WavvePrimaryText
 import org.sopt.and.ui.theme.WavveTheme
 
 @Composable
@@ -46,6 +50,7 @@ fun FilledTextField(
     onTextLayout: (TextLayoutResult) -> Unit = {},
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
     cursorBrush: Brush = SolidColor(WavveTheme.colorScheme.accent),
+    trailingContent: @Composable (() -> Unit) = {},
 ) {
     BasicTextField(
         value = value,
@@ -64,16 +69,23 @@ fun FilledTextField(
         interactionSource = interactionSource,
         cursorBrush = cursorBrush,
     ) { innerTextField ->
-        Box(
+        Row(
             modifier = Modifier
                 .background(
                     color = color,
                     shape = RoundedCornerShape(8.dp)
                 )
                 .padding(innerPadding),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            TextFieldPlaceholder(text = if (value.isEmpty()) placeholder else "")
-            innerTextField()
+            Box(
+                modifier = Modifier,
+            ) {
+                TextFieldPlaceholder(text = if (value.isEmpty()) placeholder else "")
+                innerTextField()
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            trailingContent()
         }
     }
 }
@@ -94,6 +106,24 @@ private fun FilledTextFieldNoTextPreview() {
     FilledTextField(
         value = "",
         onValueChange = {},
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = "Placeholder"
     )
 }
+
+
+@Preview(showBackground = true)
+@Composable
+private fun FilledTextFieldWithTrailingPreview() {
+    FilledTextField(
+        value = "텍스트 입력함",
+        onValueChange = {},
+        modifier = Modifier.fillMaxWidth(),
+        trailingContent = {
+            WavvePrimaryText(
+                text = "show"
+            )
+        }
+    )
+}
+
