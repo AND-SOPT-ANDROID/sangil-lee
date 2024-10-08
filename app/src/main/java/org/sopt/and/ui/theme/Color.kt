1d2ebdfa84c0aa37343947d73ec69a2f0a2dea20
Thirfir <sangbbang31@gmail.com>
2024-10-08 16:23:22 +0900

    add: 기본 색상들 추가

--- app/src/main/java/org/sopt/and/ui/theme/Color.kt
+++ app/src/main/java/org/sopt/and/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Black10 = Color(0xFF1B1B1B)
+val Black20 = Color(0xFF252525)
+
+val Gray10 = Color(0xFF717171)
+val Gray20 = Color(0xFF989898)
+
+val Blue10 = Color(0xFF1353FA)
