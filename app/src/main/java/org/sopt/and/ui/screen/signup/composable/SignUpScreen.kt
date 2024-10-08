3fcc62c7b125c49954500323d628dc9d33f53e60
Thirfir <sangbbang31@gmail.com>
2024-10-09 01:36:44 +0900

    add: 회원가입 화면 최하단 버튼

--- app/src/main/java/org/sopt/and/ui/screen/signup/composable/SignUpScreen.kt
+++ app/src/main/java/org/sopt/and/ui/screen/signup/composable/SignUpScreen.kt
@@ -1,18 +1,33 @@
 package org.sopt.and.ui.screen.signup.composable
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import org.sopt.and.R
+import org.sopt.and.ui.component.button.FullWidthTextButton
 
 @Composable
 fun SignUpScreen(
     modifier: Modifier = Modifier
 ) {
-    SignUpInputContentView(
-        modifier = modifier.padding(top = 40.dp).padding(horizontal = 12.dp)
-    )
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        SignUpInputContentView(
+            modifier = modifier.padding(top = 40.dp).padding(horizontal = 12.dp)
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        FullWidthTextButton(
+            text = stringResource(R.string.wavve_sign_up),
+            enabled = false
+        )
+    }
 }
 
 @Composable
