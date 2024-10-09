d66a38963262629996a74c6dbcd81902ded5d0dc
Thirfir <sangbbang31@gmail.com>
2024-10-09 18:22:36 +0900

    add: 이용권 여부 Text 행

--- app/src/main/java/org/sopt/and/ui/screen/my/composable/MyContentScreen.kt
+++ app/src/main/java/org/sopt/and/ui/screen/my/composable/MyContentScreen.kt
@@ -7,11 +7,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
-import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.outlined.AccountCircle
-import androidx.compose.material.icons.outlined.KeyboardArrowLeft
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.runtime.Composable
@@ -23,7 +20,6 @@ import androidx.compose.ui.unit.dp
 import org.sopt.and.R
 import org.sopt.and.User
 import org.sopt.and.ui.component.icon.PrimaryIcon
-import org.sopt.and.ui.component.icon.SecondaryIcon
 import org.sopt.and.ui.component.image.CircularImage
 import org.sopt.and.ui.component.surface.VariantSurface
 import org.sopt.and.ui.component.text.PrimaryText
@@ -37,7 +33,9 @@ fun MyContentScreen(
         modifier = modifier
     ) {
         VariantSurface {
-            Column {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                     verticalAlignment = Alignment.CenterVertically
@@ -52,11 +50,12 @@ fun MyContentScreen(
                     )
                     Spacer(modifier = Modifier.weight(1f))
                     PrimaryIcon(
+                        modifier = Modifier.padding(end = 6.dp),
                         imageVector = Icons.Outlined.Notifications,
                         contentDescription = stringResource(R.string.notification_content_description)
                     )
                     PrimaryIcon(
-                        modifier = Modifier.padding(horizontal = 16.dp),
+                        modifier = Modifier.padding(start = 12.dp),
                         imageVector = Icons.Outlined.Settings,
                         contentDescription = stringResource(R.string.notification_content_description)
                     )
@@ -78,6 +77,31 @@ fun MyContentScreen(
                         imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
                         contentDescription = stringResource(R.string.profile_image),
 
+                    )
+                }
+            }
+        }
+
+        VariantSurface(
+            modifier = Modifier.padding(top = 1.dp).fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier.padding(horizontal = 16.dp).padding(top = 8.dp, bottom = 20.dp)
+            ) {
+                SecondaryText(
+                    text = stringResource(R.string.have_no_wavve_access),
+                )
+                Row(
+                    modifier = Modifier.padding(top = 2.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    PrimaryText(
+                        text = stringResource(R.string.pay),
+                    )
+                    PrimaryIcon(
+                        modifier = Modifier.padding(start = 4.dp),
+                        imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                        contentDescription = stringResource(R.string.profile_image)
                     )
                 }
             }
