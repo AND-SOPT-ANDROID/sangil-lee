ba4cb5a3ccad7468f64d39c3e27939c91ce6771d
Thirfir <sangbbang31@gmail.com>
2024-10-09 13:18:17 +0900

    add: Password 정규식 확장함수

--- app/src/main/java/org/sopt/and/util/RegexExtensions.kt
+++ app/src/main/java/org/sopt/and/util/RegexExtensions.kt
@@ -4,3 +4,8 @@ fun String.isValidEmail(): Boolean {
     val emailRegex = Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,6}$")
     return emailRegex.matches(this)
 }
+
+fun String.isValidPassword(): Boolean {
+    val passwordRegex = Regex("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&#.~_-])[A-Za-z\\d@$!%*?&#.~_-]{8,20}$")
+    return passwordRegex.matches(this)
+}
