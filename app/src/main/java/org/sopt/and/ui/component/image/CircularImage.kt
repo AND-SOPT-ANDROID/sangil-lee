437498deb0c1071ab7d5e10c2ec3128c799a81e0
Thirfir <sangbbang31@gmail.com>
2024-10-09 18:01:07 +0900

    add: CircularImage ImageVector 매개변수 추가

--- app/src/main/java/org/sopt/and/ui/component/image/CircularImage.kt
+++ app/src/main/java/org/sopt/and/ui/component/image/CircularImage.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.DefaultAlpha
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,6 +34,29 @@ fun CircularImage(
         alpha = alpha,
         colorFilter = colorFilter,
         painter = painter,
+        contentDescription = contentDescription,
+    )
+}
+
+@Composable
+fun CircularImage(
+    imageVector: ImageVector,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    alignment: Alignment = Alignment.Center,
+    contentScale: ContentScale = ContentScale.Crop,
+    alpha: Float = DefaultAlpha,
+    colorFilter: ColorFilter? = null
+) {
+    Image(
+        modifier = Modifier.clip(
+            shape = CircleShape
+        ).then(modifier),
+        alignment = alignment,
+        contentScale = contentScale,
+        alpha = alpha,
+        colorFilter = colorFilter,
+        imageVector = imageVector,
         contentDescription = contentDescription,
     )
 }
