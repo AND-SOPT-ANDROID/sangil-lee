eed62efe1a4c3d0e0d6f3211430977e34469ddc2
Thirfir <sangbbang31@gmail.com>
2024-10-09 14:40:33 +0900

    add: 로그인 컨텐트 뷰 연결

--- app/src/main/java/org/sopt/and/ui/screen/signin/composable/SignInContentScreen.kt
+++ app/src/main/java/org/sopt/and/ui/screen/signin/composable/SignInContentScreen.kt
@@ -2,20 +2,39 @@ package org.sopt.and.ui.screen.signin.composable
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun SignInContentScreen(
     modifier: Modifier = Modifier
 ) {
+
+    var emailInput by remember { mutableStateOf("") }
+    var passwordInput by remember { mutableStateOf("") }
+
     Column(
         modifier = modifier
             .imePadding()
             .verticalScroll(rememberScrollState())
     ) {
-
+        SignInContentView(
+            modifier = Modifier.padding(top = 36.dp),
+            emailInput = emailInput,
+            passwordInput = passwordInput,
+            onEmailInputChanged = { emailInput = it },
+            onPasswordInputChanged = { passwordInput = it },
+            onLoginButtonClicked = {
+                // TODO : Implement login button click event
+            }
+        )
     }
 }
