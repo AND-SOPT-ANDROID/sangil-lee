b2242398724817332cee5dbc3e05795501202595
Thirfir <sangbbang31@gmail.com>
2024-10-09 17:12:57 +0900

    add: 회원가입 실패 Toast

--- app/src/main/java/org/sopt/and/ui/screen/signup/composable/SignUpScreen.kt
+++ app/src/main/java/org/sopt/and/ui/screen/signup/composable/SignUpScreen.kt
@@ -1,5 +1,6 @@
 package org.sopt.and.ui.screen.signup.composable
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.imePadding
@@ -13,9 +14,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import org.sopt.and.R
 import org.sopt.and.ui.component.button.FullWidthTextButton
 import org.sopt.and.util.isValidEmail
@@ -27,10 +30,15 @@ fun SignUpScreen(
     onSignUpComplete: (String, String) -> Unit = { _, _ -> }
 ) {
 
+    val context = LocalContext.current
+
     var emailInput by remember { mutableStateOf("") }
     var passwordInput by remember { mutableStateOf("") }
 
     var signUpButtonEnabled by remember { mutableStateOf(false) }
+
+    var signUpFailureMessage by remember { mutableStateOf("") }
+    var toast = Toast.makeText(context, signUpFailureMessage, Toast.LENGTH_SHORT)
 
     Column(
         modifier = Modifier
@@ -58,13 +66,32 @@ fun SignUpScreen(
             text = stringResource(R.string.wavve_sign_up),
             enabled = signUpButtonEnabled,
             onClick = {
-                onSignUpComplete(emailInput, passwordInput)
+                if (signUpButtonEnabled)
+                    onSignUpComplete(emailInput, passwordInput)
+                else {
+                    when {
+                        emailInput.isValidEmail().not() -> {
+                            toast.show()
+                        }
+                        passwordInput.isValidPassword().not() -> {
+                            toast.show()
+                        }
+                    }
+                }
             }
         )
     }
 
     LaunchedEffect(key1 = emailInput, key2 = passwordInput) {
         signUpButtonEnabled = emailInput.isValidEmail() && passwordInput.isValidPassword()
+        when {
+            emailInput.isValidEmail().not() -> {
+                signUpFailureMessage = ContextCompat.getString(context, R.string.check_email_format)
+            }
+            passwordInput.isValidPassword().not() -> {
+                signUpFailureMessage = ContextCompat.getString(context, R.string.check_password_format)
+            }
+        }
     }
 }
 
