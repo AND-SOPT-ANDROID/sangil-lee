ec05983b86f41bee67bebe7cfb4f94d21a608e0f
Thirfir <sangbbang31@gmail.com>
2024-10-09 21:11:42 +0900

    refactor: 정규식 객체 생성 줄이기

--- app/src/main/java/org/sopt/and/util/RegexExtensions.kt
+++ app/src/main/java/org/sopt/and/util/RegexExtensions.kt
@@ -1,11 +1,12 @@
 package org.sopt.and.util
 
-fun String.isValidEmail(): Boolean {
-    val emailRegex = Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,6}$")
-    return emailRegex.matches(this)
+private val emailRegex by lazy {
+    Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,6}$")
 }
 
-fun String.isValidPassword(): Boolean {
-    val passwordRegex = Regex("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&#.~_-])[A-Za-z\\d@$!%*?&#.~_-]{8,20}$")
-    return passwordRegex.matches(this)
+private val passwordRegex by lazy {
+    Regex("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&#.~_-])[A-Za-z\\d@$!%*?&#.~_-]{8,20}$")
 }
+
+fun String.isValidEmail(): Boolean = emailRegex.matches(this)
+fun String.isValidPassword(): Boolean = passwordRegex.matches(this)
