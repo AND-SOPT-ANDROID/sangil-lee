1aa8d16f60d169954e222649dcd75e5aa211ffe7
Thirfir <sangbbang31@gmail.com>
2024-10-09 17:00:03 +0900

    add: 회원가입 result launcher 설정

--- app/src/main/java/org/sopt/and/ui/screen/signin/composable/SignInContentView.kt
+++ app/src/main/java/org/sopt/and/ui/screen/signin/composable/SignInContentView.kt
@@ -1,5 +1,9 @@
 package org.sopt.and.ui.screen.signin.composable
 
+import android.app.Activity
+import android.content.Intent
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -17,6 +21,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -31,6 +36,7 @@ import org.sopt.and.ui.component.text.PrimaryText
 import org.sopt.and.ui.component.text.SecondaryText
 import org.sopt.and.ui.component.text.TertiaryText
 import org.sopt.and.ui.component.textfield.FilledTextField
+import org.sopt.and.ui.screen.signup.SignUpActivity
 import org.sopt.and.ui.theme.WavveTheme
 
 @Composable
@@ -42,6 +48,15 @@ fun SignInContentView(
     onPasswordInputChanged: (String) -> Unit,
     onLoginButtonClicked: () -> Unit
 ) {
+
+    val context = LocalContext.current
+    val signUpLauncher = rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data = result.data
+            onEmailInputChanged(data?.getStringExtra("email") ?: emailInput)
+            onPasswordInputChanged(data?.getStringExtra("password") ?: passwordInput)
+        }
+    }
 
     var isPasswordVisible by remember { mutableStateOf(false) }
 
@@ -101,6 +116,9 @@ fun SignInContentView(
             SecondaryText(
                 text = stringResource(R.string.sign_up),
                 style = WavveTheme.typography.bodySmall,
+                modifier = Modifier.clickable {
+                    signUpLauncher.launch(Intent(context, SignUpActivity::class.java))
+                }
             )
         }
 
