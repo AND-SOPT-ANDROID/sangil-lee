4095a1f1c2de27501548669b2ea0e6c9c3427a53
Thirfir <sangbbang31@gmail.com>
2024-10-09 18:15:18 +0900

    add: 첫 결제 혜택 홍보 Text 행

--- app/src/main/java/org/sopt/and/ui/screen/my/composable/MyContentScreen.kt
+++ app/src/main/java/org/sopt/and/ui/screen/my/composable/MyContentScreen.kt
@@ -7,7 +7,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.KeyboardArrowLeft
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.runtime.Composable
@@ -18,10 +22,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.and.R
 import org.sopt.and.User
+import org.sopt.and.ui.component.icon.PrimaryIcon
 import org.sopt.and.ui.component.icon.SecondaryIcon
 import org.sopt.and.ui.component.image.CircularImage
 import org.sopt.and.ui.component.surface.VariantSurface
 import org.sopt.and.ui.component.text.PrimaryText
+import org.sopt.and.ui.component.text.SecondaryText
 
 @Composable
 fun MyContentScreen(
@@ -45,14 +51,33 @@ fun MyContentScreen(
                         text = User.email
                     )
                     Spacer(modifier = Modifier.weight(1f))
-                    SecondaryIcon(
+                    PrimaryIcon(
                         imageVector = Icons.Outlined.Notifications,
                         contentDescription = stringResource(R.string.notification_content_description)
                     )
-                    SecondaryIcon(
+                    PrimaryIcon(
                         modifier = Modifier.padding(horizontal = 16.dp),
                         imageVector = Icons.Outlined.Settings,
                         contentDescription = stringResource(R.string.notification_content_description)
+                    )
+                }
+
+                SecondaryText(
+                    modifier = Modifier.padding(top = 24.dp),
+                    text = stringResource(R.string.promote_first_payment),
+                )
+                Row(
+                    modifier = Modifier.padding(top = 2.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    PrimaryText(
+                        text = stringResource(R.string.pay),
+                    )
+                    PrimaryIcon(
+                        modifier = Modifier.padding(start = 4.dp),
+                        imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                        contentDescription = stringResource(R.string.profile_image),
+
                     )
                 }
             }
