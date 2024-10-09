98444cbb272cef51bd72211a4bf0ed03dab7e769
Thirfir <sangbbang31@gmail.com>
2024-10-09 21:17:30 +0900

    refactor: 리플없는 clickable 구현 변경

--- app/src/main/java/org/sopt/and/util/ComposeExtensions.kt
+++ app/src/main/java/org/sopt/and/util/ComposeExtensions.kt
@@ -2,13 +2,22 @@ package org.sopt.and.util
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
+import androidx.compose.ui.semantics.Role
 
-inline fun Modifier.noRippleClickable(crossinline onClick: () -> Unit): Modifier = composed {
-    clickable(indication = null,
-        interactionSource = remember { MutableInteractionSource() }) {
-        onClick()
-    }
-}
+@Composable
+fun Modifier.noRippleClickable(
+    enabled: Boolean = true,
+    onClickLabel: String? = null,
+    role: Role? = null,
+    onClick: () -> Unit
+): Modifier = this.clickable(
+    indication = null,
+    interactionSource = remember { MutableInteractionSource() },
+    onClick = onClick,
+    enabled = enabled,
+    onClickLabel = onClickLabel,
+    role = role
+)
