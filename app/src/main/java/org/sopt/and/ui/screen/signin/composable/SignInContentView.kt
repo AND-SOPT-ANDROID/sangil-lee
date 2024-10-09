1fb7197ca45cc82256dccf03450a741078f746d8
Thirfir <sangbbang31@gmail.com>
2024-10-09 14:08:22 +0900

    add: 로그인 컨텐트뷰 컴포저블

---
+++ app/src/main/java/org/sopt/and/ui/screen/signin/composable/SignInContentView.kt
@@ -0,0 +1,19 @@
+package org.sopt.and.ui.screen.signin.composable
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.sopt.and.ui.component.textfield.FilledTextField
+
+@Composable
+fun SignInContentView(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+        FilledTextField(
+
+        )
+    }
+}
