6cc9eb2258175e39eaad1f76f8b98b5dae2d2183
Thirfir <sangbbang31@gmail.com>
2024-10-09 14:10:51 +0900

    add: 이메일, 비밀번호 입력필드 및 호이스팅

--- app/src/main/java/org/sopt/and/ui/screen/signin/composable/SignInContentView.kt
+++ app/src/main/java/org/sopt/and/ui/screen/signin/composable/SignInContentView.kt
@@ -1,19 +1,32 @@
 package org.sopt.and.ui.screen.signin.composable
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.sopt.and.ui.component.textfield.FilledTextField
 
 @Composable
 fun SignInContentView(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    emailInput: String,
+    passwordInput: String,
+    onEmailInputChanged: (String) -> Unit,
+    onPasswordInputChanged: (String) -> Unit,
+    onLoginButtonClicked: () -> Unit
 ) {
     Column(
         modifier = modifier
     ) {
         FilledTextField(
-
+            value = emailInput,
+            onValueChange = onEmailInputChanged
+        )
+        FilledTextField(
+            modifier = Modifier.padding(top = 6.dp),
+            value = passwordInput,
+            onValueChange = onPasswordInputChanged
         )
     }
 }
