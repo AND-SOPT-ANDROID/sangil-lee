dbd6e0d25516dcb034f5cee282bffa4ad8f8c25f
Thirfir <sangbbang31@gmail.com>
2024-10-09 17:18:46 +0900

    add: SignInActivity에 화면 연결

--- app/src/main/java/org/sopt/and/ui/screen/signin/SignInActivity.kt
+++ app/src/main/java/org/sopt/and/ui/screen/signin/SignInActivity.kt
@@ -5,17 +5,49 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import org.sopt.and.R
+import org.sopt.and.ui.component.surface.WavveDefaultSurface
+import org.sopt.and.ui.component.text.PrimaryText
+import org.sopt.and.ui.screen.signin.composable.SignInContentScreen
 import org.sopt.and.ui.theme.ANDANDROIDTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class SignInActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ANDANDROIDTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        CenterAlignedTopAppBar(
+                            title = {
+                                PrimaryText(stringResource(R.string.wavve))
+                            }, navigationIcon = {
+                                Icon(
+                                    imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
+                                    contentDescription = stringResource(R.string.navigate_up_content_description)
+                                )
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+                    WavveDefaultSurface {
+                        SignInContentScreen(
+                            modifier = Modifier.padding(innerPadding).fillMaxSize().padding(horizontal = 14.dp)
+                        )
+                    }
                 }
             }
         }
