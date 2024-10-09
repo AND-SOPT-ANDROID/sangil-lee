403f331c43abd4f116f7701136decc8f425a317a
Thirfir <sangbbang31@gmail.com>
2024-10-09 13:52:06 +0900

    add: SignInActivity

---
+++ app/src/main/java/org/sopt/and/ui/screen/signin/SignInActivity.kt
@@ -0,0 +1,23 @@
+package org.sopt.and.ui.screen.signin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import org.sopt.and.ui.theme.ANDANDROIDTheme
+
+class SignInActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ANDANDROIDTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                }
+            }
+        }
+    }
+}
