3ee84a4d290a537d7b719ec0df782d4d80364bb7
Thirfir <sangbbang31@gmail.com>
2024-10-13 19:28:21 +0900

    add: Compose Navigation lib 의존성

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
 
     implementation(project(":domain"))
 
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
