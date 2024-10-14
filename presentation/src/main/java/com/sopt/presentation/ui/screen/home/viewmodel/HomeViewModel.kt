0ed047d09c71f7983edbf70bddbb0ff0bed7a5d4
Thirfir <sangbbang31@gmail.com>
2024-10-14 22:54:32 +0900

    add: HomeViewModel

---
+++ presentation/src/main/java/com/sopt/presentation/ui/screen/home/viewmodel/HomeViewModel.kt
@@ -0,0 +1,11 @@
+package com.sopt.presentation.ui.screen.home.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel: ViewModel() {
+
+
+    companion object {
+        const val HEAD_DISPLAYED_VIDEO_COUNT = 3
+    }
+}
