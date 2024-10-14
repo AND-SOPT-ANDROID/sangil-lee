659fca07a579874f6c5d6fcf0dc1b8df38c60d7d
Thirfir <sangbbang31@gmail.com>
2024-10-14 15:45:55 +0900

    add: HomeContentScreen to HomeScreen

--- presentation/src/main/java/com/sopt/presentation/ui/screen/home/HomeScreen.kt
+++ presentation/src/main/java/com/sopt/presentation/ui/screen/home/HomeScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.sopt.presentation.ui.component.image.WavveLogoImage
 import com.sopt.presentation.ui.component.surface.WavveDefaultSurface
 import com.sopt.presentation.ui.component.top.DefaultTopAppBar
@@ -27,7 +28,9 @@ fun HomeScreen(
         WavveDefaultSurface(
             modifier = Modifier.padding(innerPadding)
         ) {
-            
+            HomeContentScreen(
+                modifier = Modifier.padding(top = 4.dp)
+            )
         }
     }
 }
