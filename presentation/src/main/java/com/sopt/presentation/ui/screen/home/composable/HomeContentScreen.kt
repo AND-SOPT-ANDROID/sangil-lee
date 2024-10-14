dd3881b94dc9429b073045cc000e09ca0e0b7893
Thirfir <sangbbang31@gmail.com>
2024-10-15 01:29:51 +0900

    refactor: Topbar를 Scaffold를 사용하지 않는 것으로 변경

--- presentation/src/main/java/com/sopt/presentation/ui/screen/home/composable/HomeContentScreen.kt
+++ presentation/src/main/java/com/sopt/presentation/ui/screen/home/composable/HomeContentScreen.kt
@@ -1,51 +1,80 @@
 package com.sopt.presentation.ui.screen.home.composable
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.sopt.presentation.ui.component.image.WavveLogoImage
 import com.sopt.presentation.ui.component.text.SecondaryText
+import com.sopt.presentation.ui.component.top.DefaultTopAppBar
 import com.sopt.presentation.ui.screen.home.type.VideoType
+import com.sopt.presentation.ui.state.CommonVideoOverviewsViewState
 import com.sopt.presentation.ui.state.VideoOverviewViewState
 import com.sopt.presentation.ui.theme.WavveTheme
 import com.sopt.presentation.util.noRippleClickable
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun HomeContentScreen(
     modifier: Modifier = Modifier,
     headVideoOverviews: List<VideoOverviewViewState>,
+    commonVideoOverviews: List<CommonVideoOverviewsViewState>,
     onVideoTypeSelected: (VideoType) -> Unit,
-    headDisplayedVideoCount: Int = 3
 ) {
 
-    val headDisplayPagerState = rememberPagerState { headDisplayedVideoCount }
+    val headDisplayPagerState = rememberPagerState { headVideoOverviews.size }
 
     LazyColumn(
         modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
+        item {
+            DefaultTopAppBar(
+                title = {
+                    WavveLogoImage()
+                }
+            )
+        }
+
         stickyHeader {
             VideoTypeTabRow(
-                modifier = Modifier.padding(horizontal = 16.dp),
+                modifier = Modifier
+                    .background(WavveTheme.colorScheme.background)
+                    .padding(horizontal = 16.dp)
+                    .padding(top = 4.dp, bottom = 12.dp),
                 onVideoTypeSelected = onVideoTypeSelected
             )
         }
 
         item {
             HeadDisplayedVideoHorizontalPager(
-                modifier = Modifier.fillMaxWidth().height(500.dp).padding(top = 16.dp),
+                modifier = Modifier.fillMaxWidth().height(500.dp),
                 state = headDisplayPagerState,
                 videoOverviews = headVideoOverviews,
+                onVideoClicked = { }
+            )
+        }
+
+        itemsIndexed(commonVideoOverviews) { _, commonVideoOverview ->
+            Spacer(modifier = Modifier.height(12.dp))
+            CommonVideosHorizontalPager(
+                modifier = Modifier.fillMaxWidth(),
+                commonVideoOverview = commonVideoOverview,
                 onVideoClicked = { }
             )
         }
@@ -65,7 +94,8 @@ private fun VideoTypeTabRow(
             SecondaryText(
                 modifier = Modifier.noRippleClickable(onClick = { onVideoTypeSelected(it) }),
                 text = stringResource(it.titleResId),
-                style = WavveTheme.typography.bodyLarge
+                style = WavveTheme.typography.bodyLarge,
+                fontWeight = FontWeight.SemiBold
             )
         }
     }
--- presentation/src/main/java/com/sopt/presentation/ui/screen/home/composable/HomeScreen.kt
+++ presentation/src/main/java/com/sopt/presentation/ui/screen/home/composable/HomeScreen.kt
@@ -1,15 +1,13 @@
 package com.sopt.presentation.ui.screen.home.composable
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.sopt.presentation.ui.component.image.WavveLogoImage
 import com.sopt.presentation.ui.component.surface.WavveDefaultSurface
-import com.sopt.presentation.ui.component.top.DefaultTopAppBar
 import com.sopt.presentation.ui.screen.home.viewmodel.HomeViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -18,25 +16,15 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = viewModel()
 ) {
-    Scaffold(
-        modifier = modifier,
-        topBar = {
-            DefaultTopAppBar(
-                title = {
-                    WavveLogoImage()
-                }
-            )
-        }
-    ) { innerPadding ->
-        WavveDefaultSurface(
-            modifier = Modifier.padding(innerPadding)
-        ) {
-            HomeContentScreen(
-                modifier = Modifier.padding(top = 4.dp),
-                headDisplayedVideoCount = HomeViewModel.HEAD_DISPLAYED_VIDEO_COUNT,
-                headVideoOverviews = viewModel.headVideoOverviews,
-                onVideoTypeSelected = { TODO() },
-            )
-        }
+    WavveDefaultSurface(
+        modifier = modifier
+            .statusBarsPadding()
+    ) {
+        HomeContentScreen(
+            modifier = Modifier.padding(top = 4.dp),
+            headVideoOverviews = viewModel.headVideoOverviews,
+            commonVideoOverviews = viewModel.commonVideoOverviews,
+            onVideoTypeSelected = { TODO() },
+        )
     }
 }
