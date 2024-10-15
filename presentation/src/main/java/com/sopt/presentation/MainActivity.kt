2c1f98c35a2078b38eddb680a34ced3efe234a11
Thirfir <sangbbang31@gmail.com>
2024-10-15 17:51:33 +0900

    chore: 불필요한 코드

--- presentation/src/main/java/com/sopt/presentation/MainActivity.kt
+++ presentation/src/main/java/com/sopt/presentation/MainActivity.kt
@@ -28,19 +28,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ANDANDROIDTheme {
-        Greeting("Android")
-    }
-}
