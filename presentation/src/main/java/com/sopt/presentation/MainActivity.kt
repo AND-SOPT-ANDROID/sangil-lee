cea201125ffbbd3ab0fa3a43b71f7f3768e04389
Thirfir <sangbbang31@gmail.com>
2024-10-16 01:16:14 +0900

    add: Statusbar color

--- presentation/src/main/java/com/sopt/presentation/MainActivity.kt
+++ presentation/src/main/java/com/sopt/presentation/MainActivity.kt
@@ -2,21 +2,26 @@ package com.sopt.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.navigation.compose.rememberNavController
 import com.sopt.presentation.ui.navigation.WavveNavigation
 import com.sopt.presentation.ui.theme.ANDANDROIDTheme
+import com.sopt.presentation.ui.theme.Black10
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(
+                Black10.toArgb()
+            )
+        )
         setContent {
             ANDANDROIDTheme {
                 val navController = rememberNavController()
