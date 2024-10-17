e22a1916d5e58280d15af0a5ea916d6cc99fda34
Thirfir <sangbbang31@gmail.com>
2024-10-17 15:56:09 +0900

    add: Search Screen, ViewModel

---
+++ presentation/src/main/java/com/sopt/presentation/ui/screen/search/composable/SearchScreen.kt
@@ -0,0 +1,14 @@
+package com.sopt.presentation.ui.screen.search.composable
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.sopt.presentation.ui.screen.search.viewmodel.SearchViewModel
+
+@Composable
+fun SearchScreen(
+    modifier: Modifier = Modifier,
+    searchViewModel: SearchViewModel = viewModel()
+) {
+
+}
---
+++ presentation/src/main/java/com/sopt/presentation/ui/screen/search/viewmodel/SearchViewModel.kt
@@ -0,0 +1,8 @@
+package com.sopt.presentation.ui.screen.search.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel(): ViewModel() {
+
+
+}
