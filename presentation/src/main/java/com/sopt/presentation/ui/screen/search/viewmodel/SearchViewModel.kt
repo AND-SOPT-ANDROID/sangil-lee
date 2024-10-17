015f31da783d03a9e706a7f51268a2b3d8647326
Thirfir <sangbbang31@gmail.com>
2024-10-17 16:49:51 +0900

    add: Search Query 뷰모델

--- presentation/src/main/java/com/sopt/presentation/ui/screen/search/viewmodel/SearchViewModel.kt
+++ presentation/src/main/java/com/sopt/presentation/ui/screen/search/viewmodel/SearchViewModel.kt
@@ -1,8 +1,19 @@
 package com.sopt.presentation.ui.screen.search.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-class SearchViewModel(): ViewModel() {
+class SearchViewModel(
+    val savedStateHandle: SavedStateHandle
+): ViewModel() {
 
+    val searchQuery = savedStateHandle.getStateFlow(SEARCH_QUERY, "")
 
+    fun onSearchQueryChanged(query: String) {
+        savedStateHandle[SEARCH_QUERY] = query
+    }
+    
+    companion object {
+        const val SEARCH_QUERY = "searchQuery"
+    }
 }
