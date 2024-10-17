bd6a85a2e8d13594fee4b3688a96d3463a57deac
Thirfir <sangbbang31@gmail.com>
2024-10-17 17:16:19 +0900

    add: DefaultRoundedChip Click 매개변수

--- presentation/src/main/java/com/sopt/presentation/ui/component/chip/DefaultRoundedChip.kt
+++ presentation/src/main/java/com/sopt/presentation/ui/component/chip/DefaultRoundedChip.kt
@@ -3,6 +3,7 @@ package com.sopt.presentation.ui.component.chip
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
@@ -16,30 +17,36 @@ import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sopt.presentation.ui.component.text.PrimaryText
 import com.sopt.presentation.ui.theme.WavveTheme
+import com.sopt.presentation.ui.util.noRippleClickable
 
 @Composable
 fun DefaultRoundedChip(
     modifier: Modifier = Modifier,
     contentPadding: PaddingValues = PaddingValues(horizontal = 8.dp, vertical = 4.dp),
+    onClick: () -> Unit = {},
+    showClickRipple: Boolean = false,
     border: BorderStroke = BorderStroke(1.dp, WavveTheme.colorScheme.tertiary),
     backgroundColor: Color = WavveTheme.colorScheme.surface,
     content: @Composable RowScope.() -> Unit
 ) {
     Row(
         modifier = modifier
+            .clip(RoundedCornerShape(50))
             .background(
                 color = backgroundColor,
                 shape = RoundedCornerShape(50)
             ).border(
                 border,
                 shape = RoundedCornerShape(50)
-            )
-            .padding(contentPadding),
+            ).then(
+                if (showClickRipple) Modifier.clickable(onClick = onClick) else Modifier.noRippleClickable(onClick = onClick)
+            ).padding(contentPadding),
         verticalAlignment = Alignment.CenterVertically
     ) {
         content()
