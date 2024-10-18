428b82dfe1294978f1047e8fc97dce90ae12c26f
Thirfir <sangbbang31@gmail.com>
2024-10-18 17:04:36 +0900

    add: UserRepository + Hilt Module

---
+++ app/src/main/java/org/sopt/and/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package org.sopt.and.di
+
+import com.sopt.data.repository.UserRepositoryImpl
+import com.sopt.domain.repository.UserRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun provideUserRepository(
+        userRepositoryImpl: UserRepositoryImpl
+    ): UserRepository
+}
