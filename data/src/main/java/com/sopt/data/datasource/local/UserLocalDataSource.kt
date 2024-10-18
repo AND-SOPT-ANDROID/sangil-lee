c02ad7f65ed600c9967f75554abfde3a43cb3478
Thirfir <sangbbang31@gmail.com>
2024-10-18 17:00:51 +0900

    add: UserLocalDataSource

---
+++ data/src/main/java/com/sopt/data/datasource/local/UserLocalDataSource.kt
@@ -0,0 +1,35 @@
+package com.sopt.data.datasource.local
+
+import android.content.SharedPreferences
+import com.sopt.domain.exception.SignInError
+import javax.inject.Inject
+
+class UserLocalDataSource @Inject constructor(
+    private val userSharedPreferences: SharedPreferences
+) {
+
+    fun saveAccount(email: String, password: String) {
+        userSharedPreferences.edit()
+            .putString(KEY_EMAIL, email)
+            .putString(KEY_PASSWORD, password)
+            .apply()
+    }
+
+    fun trySignIn(email: String, password: String): Result<Unit> {
+        return runCatching {
+            if (email == userSharedPreferences.getString(KEY_EMAIL, null) &&
+                password == userSharedPreferences.getString(KEY_PASSWORD, null)
+            )
+                Result.success(Unit)
+            else if (email != userSharedPreferences.getString(KEY_EMAIL, null))
+                Result.failure(SignInError.InvalidEmail())
+            else
+                Result.failure(SignInError.InvalidPassword())
+        }
+    }
+
+    companion object {
+        private const val KEY_EMAIL = "email"
+        private const val KEY_PASSWORD = "password"
+    }
+}
