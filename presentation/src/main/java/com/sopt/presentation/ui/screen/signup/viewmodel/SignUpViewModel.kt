e0f34e4234a6438c970b6fe5f467721e3c0e35a3
Thirfir <sangbbang31@gmail.com>
2024-10-18 17:41:53 +0900

    add: 회원가입 뷰모델 로직

--- presentation/src/main/java/com/sopt/presentation/ui/screen/signup/viewmodel/SignUpViewModel.kt
+++ presentation/src/main/java/com/sopt/presentation/ui/screen/signup/viewmodel/SignUpViewModel.kt
@@ -2,13 +2,28 @@ package com.sopt.presentation.ui.screen.signup.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sopt.domain.exception.SignUpError
+import com.sopt.domain.usecase.SignUpAccountUseCase
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.shareIn
 
 class SignUpViewModel(
-    val savedStateHandle: SavedStateHandle
+    private val savedStateHandle: SavedStateHandle,
+    private val signUpAccountUseCase: SignUpAccountUseCase
 ) : ViewModel() {
 
     val email = savedStateHandle.getStateFlow(EMAIL, "")
     val password = savedStateHandle.getStateFlow(PASSWORD, "")
+
+    private val _signUpUiState =
+        MutableSharedFlow<SignUpUiState>(replay = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
+    val signUpUiState = _signUpUiState.shareIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000)
+    )
 
     fun onEmailInputChanged(email: String) {
         savedStateHandle[EMAIL] = email
@@ -18,8 +33,29 @@ class SignUpViewModel(
         savedStateHandle[PASSWORD] = password
     }
 
+    fun signUp() {
+        signUpAccountUseCase(email.value, password.value).onSuccess {
+            _signUpUiState.tryEmit(SignUpUiState.Success)
+        }.onFailure {
+            when (it) {
+                is SignUpError.EmailInputEmpty -> _signUpUiState.tryEmit(SignUpUiState.EmailInputEmpty)
+                is SignUpError.PasswordInputEmpty -> _signUpUiState.tryEmit(SignUpUiState.PasswordInputEmpty)
+                is SignUpError.InvalidEmail -> _signUpUiState.tryEmit(SignUpUiState.InvalidEmail)
+                is SignUpError.InvalidPassword -> _signUpUiState.tryEmit(SignUpUiState.InvalidPassword)
+            }
+        }
+    }
+
     companion object {
         private const val EMAIL = "email"
         private const val PASSWORD = "password"
     }
+}
+
+sealed interface SignUpUiState {
+    data object Success : SignUpUiState
+    data object EmailInputEmpty : SignUpUiState
+    data object PasswordInputEmpty : SignUpUiState
+    data object InvalidEmail : SignUpUiState
+    data object InvalidPassword : SignUpUiState
 }
