1f00371c4c6907533148e0a18cbf3e11c5be9c84
Thirfir <sangbbang31@gmail.com>
2024-10-28 18:30:36 +0900

    add: Parcelize 플러그인

--- presentation/src/main/java/com/sopt/presentation/ui/state/VideoOverviewViewState.kt
+++ presentation/src/main/java/com/sopt/presentation/ui/state/VideoOverviewViewState.kt
@@ -1,11 +1,26 @@
 package com.sopt.presentation.ui.state
 
+import android.os.Parcelable
 import androidx.compose.runtime.Immutable
+import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
 
+@Serializable
+@Parcelize
 @Immutable
 data class VideoOverviewViewState(
     val id: Int,
     val title: String,
     val titleImage: String,
     val description: String,
-)
+): Parcelable {
+
+    companion object {
+        val Empty = VideoOverviewViewState(
+            id = 0,
+            title = "",
+            titleImage = "",
+            description = "",
+        )
+    }
+}
