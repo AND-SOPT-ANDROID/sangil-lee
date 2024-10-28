d9549fb2ae3feaa53df13984f276deaec017bd34
Thirfir <sangbbang31@gmail.com>
2024-10-28 16:51:43 +0900

    add: 기본 바텀 시트 컴포넌트

---
+++ presentation/src/main/java/com/sopt/presentation/ui/component/sheet/DefaultModalBottomSheet.kt
@@ -0,0 +1,44 @@
+package com.sopt.presentation.ui.component.sheet
+
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.sopt.presentation.ui.theme.WavveTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DefaultModalBottomSheet(
+    onDismissRequest: () -> Unit,
+    modifier: Modifier = Modifier,
+    sheetState: SheetState = rememberModalBottomSheetState(),
+    sheetMaxWidth: Dp = BottomSheetDefaults.SheetMaxWidth,
+    shape: Shape = BottomSheetDefaults.ExpandedShape,
+    containerColor: Color = WavveTheme.colorScheme.background,
+    contentColor: Color = WavveTheme.colorScheme.primary,
+    tonalElevation: Dp = 0.dp,
+    scrimColor: Color = BottomSheetDefaults.ScrimColor,
+    content: @Composable (ColumnScope.() -> Unit)
+) {
+
+    ModalBottomSheet(
+        onDismissRequest = onDismissRequest,
+        modifier = modifier,
+        sheetState = sheetState,
+        sheetMaxWidth = sheetMaxWidth,
+        shape = shape,
+        containerColor = containerColor,
+        contentColor = contentColor,
+        tonalElevation = tonalElevation,
+        scrimColor = scrimColor,
+        content = content
+    )
+}
